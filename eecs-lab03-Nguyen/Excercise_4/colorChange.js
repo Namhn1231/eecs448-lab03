e1f1bd903e9ca64ab126c9b9611e1d03b43c7285
nam nguyen <n675n055@ku.edu>
2018-10-02 12:30:35 -0500

    Added the excercies 2 and 3, but 4 is incomplete

---
+++ eecs-lab03-Nguyen/Excercise_4/colorChange.js
@@ -0,0 +1,11 @@
+function colorChange(){
+    let redBorder = document.getElementById("redBorder").value;
+    let blueBorder = document.getElementById("blueBorder").value;
+    let greenBorder = document.getElementById("greenBorder").value;
+    let widthBorder = document.getElementById("width").value;
+    let redBack = document.getElementById("redBack").value;
+    let blueBack = document.getElementById("blueBack").value;
+    let greenBack = document.getElementById("greenBack").value;
+    
+  //  $("p").css("backgroundColor = "rgb(redBack + ", " + blueBack + ", " + greenBack + ",");
+}
